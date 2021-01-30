d678bf207cfca7af15558c76b5ff96f37e74fe51
rossovitale <ilcolorevitale@gmail.com>
2021-01-30 21:09:40 +0100

    step per mm regulation

--- Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 96.2 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.21, 79.4, 400.8, 422.86 }
 
 /**
  * Default Max Feed Rate (mm/s)
