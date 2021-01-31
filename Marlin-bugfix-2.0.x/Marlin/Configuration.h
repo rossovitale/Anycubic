b692466636461538dd97fac15dd1d47824b2266c
rossovitale <ilcolorevitale@gmail.com>
2021-01-31 18:03:37 +0100

    tweak step per mm

--- Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.21, 79.4, 400.8, 422.86 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.91, 81.1, 405, 422.86 }
 
 /**
  * Default Max Feed Rate (mm/s)
